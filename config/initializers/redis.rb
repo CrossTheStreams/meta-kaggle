fe019b6195365267be18dce77bbd1c7fab62276f
Andrew <arhautau@gmail.com>
2013-11-02 18:00:53 -0700

    clean assets

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,8 @@
 if Rails.env == 'production'
-  uri = URI.parse(ENV["REDISTOGO_URL"])
-  REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-  Resque.redis = REDIS
+  uri = URI.parse(ENV["REDISTOGO_URL"]) rescue nil
+  if !.uri.nil?
+    REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+    Resque.redis = REDIS
+  end
 end
 
