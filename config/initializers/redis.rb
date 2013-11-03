0a5e8ec31c0c578addc4b827025e9349c919d205
Andrew <arhautau@gmail.com>
2013-11-02 17:36:27 -0700

    inspect the ENV global

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,5 @@
 if Rails.env == 'production'
-  Rails.logger.info("+++ #{ENV["REDISTOGO_URL"]} +++")
+  Rails.logger.info("+++ #{ENV.inspect} +++")
   uri_string = "redis://redistogo:c904c8b6174eb61be226cfc8c79e5298@beardfish.redistogo.com:10700/"
   uri = URI.parse(uri_string) rescue nil
   REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
