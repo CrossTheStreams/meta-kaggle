6a9518ded98c71a2802034d09821c018b0130768
Andrew <arhautau@gmail.com>
2013-11-02 17:21:25 -0700

    parse the string into uri obj

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,7 +1,7 @@
 if Rails.env == 'production'
   Rails.logger.info("+++ #{ENV["REDISTOGO_URL"]} +++")
-  uri = URI.parse(ENV["REDISTOGO_URL"]) rescue nil
-  uri = "redis://redistogo:c904c8b6174eb61be226cfc8c79e5298@beardfish.redistogo.com:10700/"
+  uri_string = "redis://redistogo:c904c8b6174eb61be226cfc8c79e5298@beardfish.redistogo.com:10700/"
+  uri = URI.parse(uri_string) rescue nil
   REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
   Resque.redis = REDIS
 end
