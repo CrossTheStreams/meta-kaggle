7550c8cc9afb98824a348d783d2a9f178a0c4453
Andrew <arhautau@gmail.com>
2013-11-02 17:14:37 -0700

    see whats going on

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,7 @@
 if Rails.env == 'production'
-  uri = URI.parse(URI.encode(ENV["REDISTOGO_URL"]))
+  Rails.logger.info("+++ #{ENV["REDISTOGO_URL"]} +++")
+  uri = URI.parse(ENV["REDISTOGO_URL"]) rescue nil
+  uri = "redis://redistogo:c904c8b6174eb61be226cfc8c79e5298@beardfish.redistogo.com:10700/"
   REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
   Resque.redis = REDIS
 end
