6826432df95efdabd2117529066a22c15e68e038
Andrew <arhautau@gmail.com>
2013-11-02 18:03:01 -0700

    workacation syntax error..

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,6 @@
 if Rails.env == 'production'
   uri = URI.parse(ENV["REDISTOGO_URL"]) rescue nil
-  if !.uri.nil?
+  if !uri.nil?
     REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
     Resque.redis = REDIS
   end
