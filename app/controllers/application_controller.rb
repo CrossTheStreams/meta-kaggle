74ef47e21152b61e7810f23e45a87c86018eafbf
Andrew <arhautau@gmail.com>
2013-11-14 19:59:17 -0800

    basic http auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :authenticate
+
+  protected
+
+  def authenticate
+    if Rails.env == 'production'
+      authenticate_or_request_with_http_basic do |username, password|
+        username == ENV["HTTP_USER"] && password == ENV["HTTP_PW"]
+      end
+    end
+  end
+
 end
